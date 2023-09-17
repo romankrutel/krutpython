bbfc1394a3dfc2e9c3e2e4e608b9062e6c3f5b6c
romankrutel <138224285+romankrutel@users.noreply.github.com>
2023-09-17 19:37:54 +0300

    Add files via upload

---
+++ dzkrut2/dz2.py
@@ -0,0 +1,38 @@
+N = int(input('Введите количество монет '))
+orel = reshka = 0
+for i in range(N):
+    x = int(input('Орел(1) или решка(0)? '))
+    if x == 1:
+        orel += 1
+    else:
+        reshka += 1
+if orel < reshka:
+    print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+elif orel == reshka:
+    print(f'Количество орлов и решек одинаково, по {orel} штук')
+else:
+    print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
+
+
+x = abs(int(input('Введите первое натуральное число X ')))
+y = abs(int(input('Введите второе натуральное число Y ')))
+S = x + y
+P = x * y
+y1 = int((S + ((-S) ** 2 - 4 * P) ** 0.5) / 2)
+x1 = int((S - ((-S) ** 2 - 4 * P) ** 0.5) / 2)
+print(x1, y1)
+
+
+N = abs(int(input('Введите число N ')))
+stop = 0
+P = 2
+for i in range(N):
+    if stop != 1:
+        P = P ** i
+        if P <= N:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = N
